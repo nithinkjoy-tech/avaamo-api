d245d7866c02e7319bc16d5299fdbd730b5f9457
Nithin K Joy <nithinjoy121212@gmail.com>
2020-12-30 18:01:24 +0530

    refactored the code

--- python/scrape.py
+++ python/scrape.py
@@ -4,40 +4,50 @@ import sys
 from pathlib import Path
 
 
-path = Path(r"{}".format(sys.argv[2]))
-url = sys.argv[1]
-contents = ""
-if url:
-    response = requests.get(url)
-    contents = response.text
-else:
-    with open(path, 'r') as f:
-        contents = f.read()
+try:
+    path = Path(r"{}".format(sys.argv[2]))
+    url = sys.argv[1]
+    contents = ""
+    if url:
+        response = requests.get(url,allow_redirects=False)
+        if(response.is_redirect):
+            raise Exception("The given link is protected")
+        contents = response.text
+    else:
+        with open(path, 'r') as f:
+            contents = f.read()
 
-soup = BeautifulSoup(contents, "html.parser")
-table = soup.find_all("table")
-tbody = ""
-if url:
-    tbody = table[7].find("tbody")
-else:
-    tbody = table[0].find("tbody")
-tr = tbody.find_all("tr")
-th = tr[0].find_all("th")
+    soup = BeautifulSoup(contents, "html.parser")
+    table = soup.find_all("table")
+    if not table:
+        if(url):
+            raise Exception('There is no table available in the given link')
+        else:
+            raise Exception('There is no table available in the uploaded file')
+    tbody = ""
+    if url:
+        tbody = table[7].find("tbody")
+    else:
+        tbody = table[0].find("tbody")
+    tr = tbody.find_all("tr")
+    th = tr[0].find_all("th")
 
-data = {}
-result = []
-thdata = []
-tr.pop(0)
-for item in th:
-    thdata.append(item.get_text())
-
-for i in range(len(tr)):
-    th = tr[i].find_all(["th", "td"])
-    for (key, value) in zip(thdata, th):
-        data[key] = value.get_text()
-    result.append(data)
     data = {}
-print(result)
+    result = []
+    thdata = []
+    tr.pop(0)
+    for item in th:
+        thdata.append(item.get_text())
+
+    for i in range(len(tr)):
+        th = tr[i].find_all(["th", "td"])
+        for (key, value) in zip(thdata, th):
+            data[key] = value.get_text()
+        result.append(data)
+        data = {}
+    print(result)
 
 
-sys.stdout.flush()
+    sys.stdout.flush()
+except Exception as error:
+    print("null",error)
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,13 +7,23 @@ const validateSession = require("../middleware/validateSession");
 const uploadFile = require("../utils/uploadFiles");
 const createFolder = require("../utils/createFolder");
 const {spawn} = require("child_process");
-const deleteFolder=require("../utils/deleteFolder");
+const removeDirectory = require("../utils/deleteFolder");
+const moment = require("moment");
 
 router.get("/", [auth, validateSession], async (req, res) => {
   const user = await findUser(req.user["username"]);
   const scrapeddata = await Scrapeddata.findById(user.scrapeddata);
-  console.log(scrapeddata);
   res.send(scrapeddata["data"].length.toString());
+});
+
+router.get("/:id", [auth, validateSession], async (req, res) => {
+  console.log("here", req.params.id);
+  const user = await findUser(req.user["username"]);
+  const scrapeddata = await Scrapeddata.findById(user.scrapeddata);
+  delete scrapeddata["data"][req.params.id - 1]["data"];
+  // console.log()
+  console.log(scrapeddata["data"][req.params.id - 1]);
+  res.send(scrapeddata["data"][req.params.id - 1]);
 });
 
 router.post("/", [auth, validateSession], async (req, res) => {
@@ -21,20 +31,36 @@ router.post("/", [auth, validateSession], async (req, res) => {
   let url = "";
   let path = "";
   let uploadres = "";
-  let result=""
+  let result = "";
+  let date = moment().format("MM/DD/YYYY");
+  let finalData = {};
+
+  function deleteFolder() {
+    if (!url)
+      removeDirectory(result.path, function (error) {
+        console.log(error);
+      });
+  }
 
   async function saveToDatabase(data) {
-    let dat=data.replace(/\\n/g,"")
-    data=JSON.parse(JSON.stringify(dat))
+    let dat = data.replace(/\\n/g, "");
+    data = JSON.parse(JSON.stringify(dat));
     const scrapeddata = await Scrapeddata.findById(user.scrapeddata);
-    scrapeddata.data.push(data);
+
+    if (url) finalData["URL Link"] = url;
+    else
+      finalData["filename"] = uploadres["name"].substring(
+        14,
+        uploadres["name"].length
+      );
+    finalData["Scraped On"] = date;
+    finalData["user"] = user.username;
+    finalData["data"] = data;
+
+    scrapeddata.data.push(finalData);
     await scrapeddata.save();
 
-    if(!url){
-      deleteFolder(result.path,function(error){
-        console.log(error)
-      })
-    }
+    deleteFolder();
 
     res.send(scrapeddata["data"].length.toString());
   }
@@ -45,8 +71,14 @@ router.post("/", [auth, validateSession], async (req, res) => {
       `${url}`,
       `${path}`,
     ]);
+
     process.stdout.on("data", data => {
-      saveToDatabase(data.toString());
+      data = data.toString();
+      if (data.indexOf("null") === 0) {
+        deleteFolder();
+        return res.status(400).send(data.substring(5, data.length));
+      }
+      saveToDatabase(data);
     });
   }
 
@@ -66,13 +98,13 @@ router.post("/", [auth, validateSession], async (req, res) => {
 });
 
 router.post("/:id", [auth, validateSession], (req, res) => {
+  console.log("a", req.params.id);
   const {question} = req.body;
   if (!question)
     return res
       .status(400)
       .send({property: "question", msg: "Question should not be empty"});
 
-  
   res.send(question + "this is the answer");
 });
 
